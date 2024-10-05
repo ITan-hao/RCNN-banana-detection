dffb7a43dfdc15d8b07fc7cf7381ead17c49d01e
IT小白_安昊昊 <121411351+ITan-hao@users.noreply.github.com>
2024-10-05 19:18:51 +0800

    Update bb_regression.py

--- bb_regression.py
+++ bb_regression.py
@@ -1,4 +1,3 @@
-# 利用最后一个池化层pool输出的特征去训练一个线性回归模型，以预测新的检测框
 import utils
 import torch
 import pandas as pd
@@ -43,9 +42,8 @@ def train(net, epochs, lr, criterion, device, transform):
     train_data = Reg_dataset(ss_train_loc, gt_train_loc, transform, net=net, device=device)
     val_data = Reg_dataset(ss_val_loc, gt_val_loc, transform, net=net, device=device)
     train_dataloader = DataLoader(train_data, batch_size=128, shuffle=True)
-    val_dataloader = DataLoader(val_data, batch_size=128)#使用DataLoader创建批量训练数据加载器和验证数据加载器
+    val_dataloader = DataLoader(val_data, batch_size=128)
 
     linear_net = nn.Sequential(nn.AdaptiveAvgPool2d((6, 6)), nn.Flatten(), nn.Linear(256*6*6, 4))
     nn.init.xavier_normal_(linear_net[-1].weight)#使用Xavier初始化权重。
     utils.train(train_dataloader, val_dataloader, linear_net, epochs, lr, criterion, device, len(train_data), len(val_data), mode='regression')
-#调用utils.train函数进行模型训练，该函数包含了训练过程，包括训练循环、验证、优化器更新等。
